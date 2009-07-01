9e6705010d3f21f10e939a8d81fc777350dddedc
kig <ilmari.heikkinen@gmail.com>
2009-07-01 04:48:20 +0300

    rendergame.hs haddock comments

--- rendergame.hs
+++ rendergame.hs
@@ -15,6 +15,8 @@ drawVictory = putStrLn "VICTORY!!! HUZZAH!!! BANZAI!!!"
 type Scene = [(Matrix4x4, Model)]
 type Models = [(String, Model)]
 
+-- | Draws the current level (i.e. the equation and matching part of inventory)
+--   transformed by pMatrix.
 drawLevel :: Matrix4x4 -> AppState -> IO ()
 drawLevel pMatrix st =
     let equ = equation st
@@ -22,30 +24,43 @@ drawLevel pMatrix st =
         inv = inventory st
         invIdx = inventoryIndex st in do
     drawInventory pMatrix (inventoryFor cloc equ inv) (models st) invIdx
-    drawEquation pMatrix equ (models st) cloc
+    drawCheckableRule pMatrix equ (models st) cloc
 
-drawEquation :: Matrix4x4 -> CheckableRule -> Models -> Int -> IO ()
-drawEquation m (_,rule) models idx = drawRule m rule models idx
+-- | Draws a CheckableRule transformed by m, with the given models, equation
+--   cursor drawn at idx. Wrapper around drawRule (and drawExpr.)
+drawCheckableRule :: Matrix4x4 -> CheckableRule -> Models -> Int -> IO ()
+drawCheckableRule m (_,rule) models idx = drawRule m rule models idx
 
+-- | Draws a Rule transformed by m, with the given models, equation
+--   cursor drawn at idx. Wrapper around drawExpr.
 drawRule :: Matrix4x4 -> Rule -> Models -> Int -> IO ()
 drawRule m rule models idx = drawExpr m (toExpr rule) models idx
 
+-- | Draws an Expr transformed by m, with the given models, equation
+--   cursor drawn at idx.
+--   First calls buildScene to create the draw list, then draws it using drawScene.
 drawExpr :: Matrix4x4 -> Expr -> Models -> Int -> IO ()
 drawExpr m e models idx = drawScene m $ buildScene models e idx
 
+-- | Draws the Scene transformed by m.
+--   Maps over the Scene with drawTransformModel.
 drawScene :: Matrix4x4 -> Scene -> IO ()
 drawScene m = mapM_ (drawTransformModel m)
 
+-- | Draws a (transform, Model) -pair, transformed by m.
+--   First loads the matrix (m x transform), then draws the model.
 drawTransformModel :: Matrix4x4 -> (Matrix4x4, Model) -> IO ()
 drawTransformModel m (transform, model) = do
     glLoadMatrix (matrixMul m transform)
     drawModel model
 
+-- | Creates the Scene for an Expr, using Models, cursor drawn at position idx.
+--   The created Scene is centered around the root operator.
 buildScene :: Models -> Expr -> Int -> Scene
 buildScene models expr@(Expr (o, l, r)) idx =
-    move (-leftWidth) 0 leftModel ++ 
-    move 0 0 centerModel ++
-    move centerWidth 0 rightModel
+    move (-leftWidth-centerWidth/2) 0 leftModel ++ 
+    move (-centerWidth/2) 0 centerModel ++
+    move (centerWidth/2) 0 rightModel
     where totalWidth = centerWidth + leftWidth + rightWidth
           (centerIdx, centerWidth, centerModel) = opModel models o leftIdx
           (leftIdx, leftWidth, leftModel) = buildScene' models l idx
@@ -72,6 +87,8 @@ buildScene' models Y idx = (idx-1, 1, getModel models "Y" idx)
 buildScene' models Z idx = (idx-1, 1, getModel models "Z" idx)
 buildScene' models (Literal i) idx = (idx-1, 1, getModel models "L" idx)
 
+-- | Returns the neutral model for an operator.
+--   The neutral model is the "E" model over the operator model.
 neutral :: Models -> (Int, GLfloat, Scene) -> (Int, GLfloat, Scene)
 neutral models (i,w, (m,opm):xs) =
     (i,w, (m,nm):(m2,opm):xs)
@@ -79,6 +96,9 @@ neutral models (i,w, (m,opm):xs) =
           ms = scalingMatrix [0.75, 0.75, 0.75]
           m2 = matrixMul (translationMatrix [0.125, 0.125, 0.0]) ms
 
+-- | Returns the inverse model for an operator and variable.
+--   The inverse model is the "I" model over the variable model,
+--   with a small version of the operator model in the bottom-right corner.
 inverse :: Models -> Op -> (Int, GLfloat, Scene) -> (Int, GLfloat, Scene)
 inverse models o (i,w, (m,eqM):xs) =
     (i,w, (m,iM):(m2,eqM):(m3,opM):xs)
@@ -89,22 +109,31 @@ inverse models o (i,w, (m,eqM):xs) =
           ms2 = scalingMatrix [0.5, 0.5, 0.5]
           m3 = matrixMul (translationMatrix [0.5, -0.125, 0.0]) ms2
 
+-- | Returns the model for an operator.
 opModel :: Models -> Op -> Int -> (Int, GLfloat, Scene)
 opModel models o idx = (idx-1, 1, getModel models o idx)
 
+-- | Returns the Scene for a model in Models matching the key k.
+--   Appends the cursor model to the scene if idx is zero.
+--   The cursor model is the model with key ">".
 getModel :: Models -> String -> Int -> Scene
 getModel models k idx = 
     [(identityMatrix, lookupOrFirst k models)] ++ cursor
     where cursor = if idx == 0 then [(identityMatrix, lookupOrFirst ">" models)]
                                else []
 
+-- | Looks up the first match to k in the association list lst.
+--   Returns the first element value of lst if no match is found for k.
 lookupOrFirst :: Eq a => a -> [(a, b)] -> b
 lookupOrFirst k lst = maybe (snd $ head lst) id (lookup k lst)
 
+-- | Translates the given Scene by (x,y,0).
 move :: GLfloat -> GLfloat -> Scene -> Scene
 move x y = map (\(tr, model) -> (matrixMul m tr, model))
            where m = translationMatrix [x, y, 0]
 
+-- | Draws the inventory transformed by the camera matrix, using the given models.
+--   The inventory is scrolled to the cursor position given by idx.
 drawInventory :: Matrix4x4 -> ProofInventory -> Models -> Int -> IO ()
 drawInventory camera [] models idx = return ()
 drawInventory camera inventory models idx = do
@@ -121,16 +150,25 @@ drawInventory camera inventory models idx = do
           cameran = matrixMul camerat (scalingMatrix [0.66, 0.66, 0.66])
           camerat = matrixMul camera (translationMatrix [0, 3, 0])
 
+-- | Draws an inventory entry transformed by the camera matrix.
+--   See drawInventoryRule.
 drawInventoryEntry :: Matrix4x4 -> Models -> Int -> (GLfloat, Rule) -> IO ()
 drawInventoryEntry camera models cursorIdx (offset, rule) =
     drawInventoryRule matrix rule models cursorIdx
     where matrix = matrixMul camera tr
           tr = translationMatrix [0, 4*offset, 0]
 
+-- | Returns the length of the left side of an inventory rule.
+--   The left length of equality transforms is the length of the left side of
+--   the left-side equality (equality transforms are of form (A+C=B+C) = (A=B).)
 leftLength :: Rule -> Int
 leftLength (Rule (Expr ("=",l,_), Expr ("=",_,_))) = exprLength l
 leftLength (Rule (l, r)) = exprLength l
 
+-- | Draws the inventory rule, transformed by m.
+--   Equality transforms have only their left side drawn for clarity (e.g.
+--   from (A+C = B+C) = (A = B), only A+C = B+C is drawn.)
+--   See drawRule.
 drawInventoryRule :: Matrix4x4 -> Rule -> Models -> Int -> IO ()
 drawInventoryRule m (Rule (r, Expr("=",_,_))) models idx =
     drawRule m (maybe (Rule (A,A)) id (toRule r)) models idx
