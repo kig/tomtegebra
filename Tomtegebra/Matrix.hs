c05718c30819bc348dbea6343fd741ea0d542bf6
kig <ilmari.heikkinen@gmail.com>
2009-07-03 00:20:40 +0300

    Matrix.hs, Models.hs: haddock docs

--- Tomtegebra/Matrix.hs
+++ Tomtegebra/Matrix.hs
@@ -1,38 +1,44 @@
+{- |
+    Matrix math functions for doing the OpenGL transformation matrices in Haskell.
+-}
 module Matrix where
 
 import Data.List
 import Graphics.Rendering.OpenGL
 import Foreign.Ptr
 
+-- | 4x4 Matrix in the OpenGL orientation: translation column is the last 4 elements.
 type Matrix4x4 = [Vec4]
+-- | 3x3 Matrix in the OpenGL orientation.
 type Matrix3x3 = [Vec3]
+-- | Four element GLfloat vector.
 type Vec4 = [GLfloat]
+-- | Three element GLfloat vector.
 type Vec3 = [GLfloat]
 
--- Matrices are in the OpenGL orientation:
--- "translation column is the last 4 elements"
-
+-- | Multiplies the current OpenGL matrix with the given 'Matrix4x4'.
 glMultMatrix :: Matrix4x4 -> IO ()
 glMultMatrix m = do
     gm <- glMatrix m
     multMatrix gm
 
+-- | Loads the given 'Matrix4x4' as the current OpenGL matrix.
 glLoadMatrix :: Matrix4x4 -> IO ()
 glLoadMatrix m = do
     gm <- glMatrix m
     matrix Nothing $= gm
 
+-- | Converts the 'Matrix4x4' into a 'GLmatrix' 'GLfloat'
 glMatrix :: Matrix4x4 -> IO (GLmatrix GLfloat)
-glMatrix m = newMatrix ColumnMajor $ flatten m :: IO (GLmatrix GLfloat)
+glMatrix m = newMatrix ColumnMajor $ concat m :: IO (GLmatrix GLfloat)
 
+-- | 'withMatrix' wrapper for 'withMatrix4x4'
 withMatrix4x4 :: Matrix4x4 -> (MatrixOrder -> Ptr GLfloat -> IO a) -> IO a
 withMatrix4x4 matrix m = do
     mat <- glMatrix matrix
     withMatrix mat m
 
-flatten :: [[a]] -> [a]
-flatten = foldl1 (++)
-
+-- | The 'Matrix4x4' identity matrix.
 identityMatrix :: Matrix4x4
 identityMatrix =
     [
@@ -42,20 +48,25 @@ identityMatrix =
         [0,0,0,1]
     ]
 
+-- | Multiplies two matrices together.
 matrixMul :: Matrix4x4 -> Matrix4x4 -> Matrix4x4
 matrixMul a b =
     map (\row -> map (dotVec row) at) b
     where at = transpose a
 
+-- | Multiplies a vector by a matrix.
 matrixMulVec :: Matrix4x4 -> Vec4 -> Vec4
 matrixMulVec m v = map (dotVec v) (transpose m)
 
+-- | Returns the upper-left 3x3 matrix of a 4x4 matrix.
 matrix4x4To3x3 :: Matrix4x4 -> Matrix3x3
 matrix4x4To3x3 m = take 3 $ map vec4To3 m
 
+-- | Pads the 3x3 matrix to a 4x4 matrix with a 1 in bottom right corner and 0 elsewhere.
 matrix3x3To4x4 :: Matrix3x3 -> Matrix4x4
 matrix3x3To4x4 [x,y,z] = [x ++ [0], y ++ [0], z ++ [0], [0,0,0,1]]
 
+-- | Inverts a 4x4 orthonormal matrix with the special case trick.
 invertMatrix4x4ON :: Matrix4x4 -> Matrix4x4
 invertMatrix4x4ON m = -- orthonormal matrix inverse
     let [a,b,c] = transpose $ matrix4x4To3x3 m
@@ -66,12 +77,16 @@ invertMatrix4x4ON m = -- orthonormal matrix inverse
         [dotVec a t, dotVec b t, dotVec c t, t4 !! 3]
     ]
 
+-- | Creates the translation matrix that translates points by the given vector.
 translationMatrix :: Vec3 -> Matrix4x4
 translationMatrix [x,y,z] = [[1,0,0,0], [0,1,0,0], [0,0,1,0], [x,y,z,1]]
 
+-- | Creates the scaling matrix that scales points by the factors given by the
+--   vector components.
 scalingMatrix :: Vec3 -> Matrix4x4
 scalingMatrix [x,y,z] = [[x,0,0,0], [0,y,0,0], [0,0,z,0], [0,0,0,1]]
 
+-- | Creates a rotation matrix from the given angle and axis.
 rotationMatrix :: GLfloat -> Vec3 -> Matrix4x4
 rotationMatrix angle axis =
     let [x,y,z] = normalizeVec axis
@@ -85,6 +100,8 @@ rotationMatrix angle axis =
       [0,0,0,1]
     ]
 
+-- | Creates a lookAt matrix from three vectors: the eye position, the point the
+--   eye is looking at and the up vector of the eye.
 lookAtMatrix :: Vec3 -> Vec3 -> Vec3 -> Matrix4x4
 lookAtMatrix eye center up =
     let z = directionVec eye center in
@@ -92,6 +109,8 @@ lookAtMatrix eye center up =
     let y = normalizeVec $ crossVec3 z x in
     matrixMul (matrix3x3To4x4 $ transpose [x,y,z]) (translationMatrix (negateVec eye))
 
+-- | Creates a frustumMatrix from the given left, right, bottom, top, znear and zfar
+--   values for the view frustum.
 frustumMatrix :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> GLfloat -> GLfloat -> Matrix4x4
 frustumMatrix left right bottom top znear zfar =
     let x = 2*znear/(right-left)
@@ -107,6 +126,8 @@ frustumMatrix left right bottom top znear zfar =
       [0, 0, d, 0]
     ]
 
+-- | Creates a perspective projection matrix for the given field-of-view,
+--   screen aspect ratio, znear and zfar.
 perspectiveMatrix :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> Matrix4x4
 perspectiveMatrix fovy aspect znear zfar =
     let ymax = znear * tan (fovy * pi / 360.0) in
@@ -115,23 +136,36 @@ perspectiveMatrix fovy aspect znear zfar =
         xmax = ymax * aspect in
     frustumMatrix xmin xmax ymin ymax znear zfar
 
+-- | Normalizes a vector to a unit vector.
 normalizeVec v = scaleVec (recip $ lengthVec v) v
+-- | Scales a vector by a scalar
 scaleVec s v = map ((*) s) v
+-- | Computes the length of a vector.
 lengthVec v = sqrt.sum $ map square v
 
+-- | Inner product of two vectors.
 innerVec = zipWith (*)
+-- | Adds two vectors together.
 addVec = zipWith (+)
+-- | Subtracts a vector from another.
 subVec = zipWith (-)
+-- | Negates a vector.
 negateVec = map negate
+-- | Computes the direction unit vector between two vectors.
 directionVec u v = normalizeVec (subVec u v)
+-- | Vector dot product.
 dotVec a b = sum $ innerVec a b
+-- | Cross product of two 3-vectors.
 crossVec3 [u0,u1,u2] [v0,v1,v2] = [u1*v2-u2*v1, u2*v0-u0*v2, u0*v1-u1*v0]
 
+-- | Converts a 4-vector into a 3-vector by dropping the fourth element.
 vec4To3 :: Vec4 -> Vec3
 vec4To3 = take 3
 
+-- | Converts a 3-vector into a 4-vector by appending the given value to it.
 vec3To4 :: Vec3 -> GLfloat -> Vec4
 vec3To4 v i = v ++ [i]
 
+-- | Multiplies a GLfloat by itself.
 square :: GLfloat -> GLfloat
 square x = x * x
--- Tomtegebra/Models.hs
+++ Tomtegebra/Models.hs
@@ -1,5 +1,9 @@
+{- |
+    Helpers for creating and drawing bundled models.
+-}
 module Models where
-import Graphics.Rendering.OpenGL
+
+import Graphics.Rendering.OpenGL hiding (Height)
 import VBO
 import Texture
 import Foreign.Ptr (Ptr, castPtr)
@@ -8,97 +12,38 @@ import Data.Int
 import Data.IORef
 import System.IO.Unsafe
 import Foreign.Ptr
+import Graphics.UI.Gtk.Pango.Layout
+import Graphics.UI.Gtk.Pango.Markup (Markup)
 
+-- | 3D Vertex
 type Vert3 = (GLfloat, GLfloat, GLfloat)
+
+-- | 2D Texture coordinate
 type Tex2 = (GLfloat, GLfloat)
+
+type Width = Int
+type Height = Int
+
+-- | Model that bundles a VBO and textures together into a drawable object.
 data Model = Model {
-    mode::PrimitiveMode,
-    count::NumArrayIndices,
-    vbo::BufferObject,
-    textures::[TextureObject],
-    verts::VertexArrayDescriptor GLfloat,
-    texCoords::VertexArrayDescriptor GLfloat}
-
-data Vbo a = Vbo (NumArrayIndices, BufferObject, (IntegerHandling, VertexArrayDescriptor a))
-data Stream a = Stream (AttribLocation, Vbo a)
-data Sampler = Sampler (UniformLocation, TextureTarget, TextureObject)
-
-data UniformSetting = UniformSetting (UniformLocation, UniformValue)
-
-data UniformValue =
-      UniformMatrix4 (Matrix4x4)
-    | UniformVertex4 (Vertex4 GLfloat)
-    | UniformVertex3 (Vertex3 GLfloat)
-    | UniformVertex2 (Vertex2 GLfloat)
-    | UniformFloat (TexCoord1 GLfloat)
-    | UniformInt (TexCoord1 GLint)
-
-data Drawable = Drawable {
-    program    :: Program,
-    uniforms   :: [UniformSetting],
-    streamMode :: PrimitiveMode,
-    streams    :: [Stream GLfloat],
-    indices    :: Maybe (Vbo GLushort),
-    samplers   :: [Sampler]
+    mode::PrimitiveMode,    -- ^Mode of the vertices, e.g. TriangleFan
+    count::NumArrayIndices, -- ^Vertex count
+    vbo::BufferObject,      -- ^VBO with the vertices and texcoords
+    textures::[TextureObject], -- ^List of textures to bind with 'Texture.useTexture'
+    verts::VertexArrayDescriptor GLfloat, -- ^Vertex array descriptor for the model vertices
+    texCoords::VertexArrayDescriptor GLfloat -- ^Vertex array descriptor for the model texture coordinates
     }
 
-uniformSetting :: UniformSetting -> IO ()
-uniformSetting (UniformSetting(location, UniformMatrix4 value)) =
-    withMatrix4x4 value (\order ptr -> uniformv location 16 (castPtr ptr :: Ptr (TexCoord1 GLfloat)))
-uniformSetting (UniformSetting(location, UniformVertex4 value)) = uniform location $= value
-uniformSetting (UniformSetting(location, UniformVertex3 value)) = uniform location $= value
-uniformSetting (UniformSetting(location, UniformVertex2 value)) = uniform location $= value
-uniformSetting (UniformSetting(location, UniformFloat value)) = uniform location $= value
-uniformSetting (UniformSetting(location, UniformInt value)) = uniform location $= value
-
-drawDrawable :: Drawable -> IO ()
-drawDrawable d = do
-    currentProgram $= Just (program d)
-    setUniforms (uniforms d)
-    setSamplers (samplers d)
-    withStreams (streams d) (do
-        case indices d of
-            Just (Vbo (num, vbo, (_,VertexArrayDescriptor numcomp datatype stride ptr))) -> do
-                bindBuffer ElementArrayBuffer $= Just vbo
-                drawElements (streamMode d) num datatype ptr
-                bindBuffer ElementArrayBuffer $= Nothing
-            Nothing -> drawArrays (streamMode d) 0 (minNum (streams d)))
-    currentProgram $= Nothing
-    where minNum streams = minimum $ map (\(Stream (_,Vbo(n,_,_))) -> n) streams
-
-withStreams :: [Stream a] -> IO () -> IO ()
-withStreams streams m = do
-    setStreams streams
-    m
-    disableStreams streams
-
-setStreams :: [Stream a] -> IO ()
-setStreams streams = 
-    mapM_ (\(Stream (location, Vbo (_, vbo, value))) -> do
-            bindBuffer ArrayBuffer $= Just vbo
-            vertexAttribArray location $= Enabled
-            vertexAttribPointer location $= value) streams
-
-disableStreams :: [Stream a] -> IO ()
-disableStreams streams =
-    mapM_ (\(Stream (location,_)) -> vertexAttribArray location $= Disabled) streams
-
-setUniforms :: [UniformSetting] -> IO ()
-setUniforms uniforms = mapM_ uniformSetting uniforms
-
-setSamplers :: [Sampler] -> IO ()
-setSamplers samplers =
-    mapM_ (\(i, Sampler (location, texType, tex)) -> do
-            activeTexture $= TextureUnit i
-            textureBinding texType $= Just tex
-            uniform location $= TexCoord1 i) $ zip [0..] samplers
-
+-- | Fills screen with the current color.
+--
+--   Loads the identity matrix and disables Texture2D.
 fillScreen :: IO ()
 fillScreen = do
     glLoadMatrix identityMatrix
     texture Texture2D $= Disabled
     renderPrimitive TriangleFan $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) quadVerts
 
+-- | Draws a 'Model' to the screen.
 drawModel :: Model -> IO ()
 drawModel model = do
     bindBuffer ArrayBuffer $= Just (vbo model)
@@ -109,9 +54,13 @@ drawModel model = do
     useTexture (textures model)
     drawInstance model
 
+-- | Calls drawArrays on the 'Model' values.
+--   Useful for drawing the same model several times without state setting
+--   overhead.
 drawInstance :: Model -> IO ()
 drawInstance model = drawArrays (mode model) 0 (count model)
 
+-- | Creates a model from a list of vertices and texture coords.
 createModel :: PrimitiveMode -> [Vert3] -> [Tex2] -> IO Model
 createModel vertType verts texCoords = do
     vbo <- createVBO elems
@@ -129,55 +78,93 @@ createModel vertType verts texCoords = do
           con (x,y,z) (s,t) = [x,y,z,s,t]
           offset x = plusPtr nullPtr x
 
+-- | Vertices for a TriangleFan hexagon model centered on the origin with a radius of 1.
 hexVerts :: [(GLfloat,GLfloat,GLfloat)]
 hexVerts = map (\k -> (sin(2*pi*k/6), cos(2*pi*k/6), 0.0)) [1..6]
 
+-- | Texture coords for the hexagon model.
 hexTexCoords :: [(GLfloat, GLfloat)]
 hexTexCoords = map (\(x,y,z) -> ((x+1.0)*0.5, 1.0-(y+1.0)*0.5)) hexVerts
 
-createHexModel = createModel TriangleFan hexVerts hexTexCoords
+-- | Gets a cached copy of the hexagon model.
+getHexModel :: IO Model
+getHexModel =
+    getCachedModel hexModel (createModel TriangleFan hexVerts hexTexCoords)
 
+-- | Vertices for a TriangleFan quad model centered on the origin with a radius of 1.
 quadVerts :: [(GLfloat, GLfloat, GLfloat)]
 quadVerts = [(-1,-1,0), (-1,1,0), (1,1,0), (1,-1,0)]
+
+-- | Texture coords for the quad model.
 quadTexCoords :: [(GLfloat, GLfloat)]
 quadTexCoords = [(0,0), (0,1), (1,1), (1,0)]
 
-createQuadModel = createModel TriangleFan quadVerts quadTexCoords
+-- | Gets a cached copy of the quad model.
+getQuadModel :: IO Model
+getQuadModel =
+    getCachedModel quadModel (createModel TriangleFan quadVerts quadTexCoords)
 
+-- | Vertices for a triangle model centered on the origin with a radius of 1.
 triVerts :: [(GLfloat, GLfloat, GLfloat)]
 triVerts = [(-1,-1,0), (0,1,0), (1,-1,0)]
+
+-- | Texture coords for the triangle model.
 triTexCoords :: [(GLfloat, GLfloat)]
 triTexCoords = [(0,0), (0.5,1), (1,0)]
 
-createTriModel = createModel Triangles triVerts triTexCoords
+-- | Gets a cached copy of the triangle model.
+getTriModel :: IO Model
+getTriModel =
+    getCachedModel triModel (createModel Triangles triVerts triTexCoords)
 
+-- | Vertices for a TriangleFan quad image model with bottom-left corner on
+--   the origin and side length of 1.
 quadImageVerts :: [(GLfloat, GLfloat, GLfloat)]
 quadImageVerts = [(0,0,0), (0,1,0), (1,1,0), (1,0,0)]
 
+-- | Texture coords for the quad image model, flipped vertically so that y-down
+--   images display correctly.
 quadImageTexCoords :: [(GLfloat, GLfloat)]
 quadImageTexCoords = [(0,1), (0,0), (1,0), (1,1)]
 
-createQuadImageModel = createModel TriangleFan quadImageVerts quadImageTexCoords
+-- | Gets a cached copy of the quad image model.
+getQuadImageModel :: IO Model
+getQuadImageModel =
+    getCachedModel imageModel (createModel TriangleFan quadImageVerts quadImageTexCoords)
+
+-- | Loads an image file as an image model.
+createImageModel :: FilePath -> IO (Width, Height, Model)
+createImageModel fn = do
+    (w,h,tex) <- loadTexture fn
+    q <- getQuadImageModel
+    return (w,h,q {textures = [tex]})
+
+-- | Creates a text image model from a 'LayoutAlignment' and 'Markup'.
+createTextModel :: LayoutAlignment -> Markup -> IO (Width, Height, Model)
+createTextModel alignment markup = do
+    (w,h,tex) <- createTextTexture alignment markup
+    q <- getQuadImageModel
+    return (w,h,q {textures = [tex]})
+
+
+-- model caching
 
 imageModel = unsafePerformIO (newIORef Nothing)
 {-# NOINLINE imageModel #-}
+hexModel = unsafePerformIO (newIORef Nothing)
+{-# NOINLINE hexModel #-}
+triModel = unsafePerformIO (newIORef Nothing)
+{-# NOINLINE triModel #-}
+quadModel = unsafePerformIO (newIORef Nothing)
+{-# NOINLINE quadModel #-}
 
-getCachedImageModel = do
-    im <- get imageModel
+
+getCachedModel ioref create = do
+    im <- get ioref
     case im of
         Just q -> return q
         Nothing -> do
-            iq <- createQuadImageModel
-            imageModel $= Just iq
+            iq <- create
+            ioref $= Just iq
             return iq
 
-createImageModel fn = do
-    (w,h,tex) <- loadTexture fn
-    q <- getCachedImageModel
-    return (w,h,q {textures = [tex]})
-
-createTextModel alignment markup = do
-    (w,h,tex) <- createTextTexture alignment markup
-    q <- getCachedImageModel
-    return (w,h,q {textures = [tex]})
-    
